27a959f1d93be088b589754949abc355213a324b
dashwav <dashwav@gmail.com>
2019-10-26 16:35:10 -0700

    Test w/ yaml

--- tests/test_gila.py
+++ tests/test_gila.py
@@ -5,6 +5,13 @@ import gila
 
 class TestGila(unittest.TestCase):
 
+    def test_reset(self):
+        key = "key"
+        value = "value"
+        gila.set(key, value)
+        gila.reset()
+        self.assertIsNone(gila.get(key))
+
     def test_setting_values(self):
         key = "key"
         value = "value"
@@ -25,6 +32,13 @@ class TestGila(unittest.TestCase):
         self.assertEqual(gila.get(key), default_value)
         gila.set(key, override_value)
         self.assertEqual(gila.get(key), override_value)
+
+    def test_read_in_yaml(self):
+        gila.set_config_name('yaml_config')
+        gila.add_config_path('./tests/configs')
+        gila.read_in_config()
+        gila.debug()
+        self.assertEqual(True, gila.get("exists"))
 
 
 if __name__ == '__main__':
