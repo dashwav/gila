a1b841e432f4ce2c10ddc86fd9bfcf784caa4094
dashwav <dashwav@gmail.com>
2019-10-26 11:09:43 -0700

    Add some more tests

--- tests/test_gila.py
+++ tests/test_gila.py
@@ -12,6 +12,23 @@ class TestGila(unittest.TestCase):
         gila_instance.set(key, value)
         self.assertEqual(gila_instance.get(key), value)
 
+    def test_setting_default(self):
+        key = "key"
+        value = "value"
+        gila_instance = Gila()
+        gila_instance.set_default(key, value)
+        self.assertEqual(gila_instance.get(key), value)
+
+    def test_override_precedence_default(self):
+        key = "key"
+        default_value = "test"
+        override_value = "value"
+        gila_instance = Gila()
+        gila_instance.set_default(key, default_value)
+        self.assertEqual(gila_instance.get(key), default_value)
+        gila_instance.set(key, override_value)
+        self.assertEqual(gila_instance.get(key), override_value)
+
 
 if __name__ == '__main__':
     unittest.main()
